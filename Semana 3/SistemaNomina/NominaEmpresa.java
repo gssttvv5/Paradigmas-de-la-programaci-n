09391ee92d0db63360462f3636b1825b6eba0296
gssttvv5 <camexops11@gmail.com>
2026-08-31 11:41:46 -0600

    Diagra UML de uso de casos

--- Semana 3/SistemaNomina/NominaEmpresa.java
+++ Semana 3/SistemaNomina/NominaEmpresa.java
@@ -2,11 +2,10 @@ package SistemaNomina;
 
 public class NominaEmpresa {
     public static void main(String[] args) {
-        // Array polimórfico con 8 empleados
+
         Empleado[] nomina = new Empleado[8];
 
-        // Los sueldos base están calculados a la inversa para que al sumar 
-        // la bonificación den exactamente los números de la imagen de salida.
+
         nomina[0] = new Gerente("Laura", "111", 4000.00);
         nomina[1] = new Gerente("Roberto", "222", 4500.00);
         nomina[2] = new Supervisor("Ana", "333", 2500.00);
@@ -18,13 +17,11 @@ public class NominaEmpresa {
 
         double totalPagar = 0;
 
-        // Mostrar tabla de nómina con ligadura dinámica
         for (Empleado emp : nomina) {
             System.out.println(emp.obtenerDetalles());
             totalPagar += emp.calcularSueldo();
         }
 
-        // Mostrar estadísticas usando métodos estáticos
         Empleado.mostrarEstadisticas(totalPagar);
     }
 }
